7d7f79ad9b3ce4aa3a7411291d1ad043874ff299
Ian Pollack <ianjpollack@gmail.com>
2018-03-21 21:16:02 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,15 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
+    numberedLine = []
+    q = 1
+    i = 0
+    loop do
+      numberedLine.push("#{q}. #{katz_deli[i]}")
+    end
+
+    currentNumberedLine.push()
+
     puts katz_deli
   end
 end
